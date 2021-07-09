24a10303d410ca8a247fce198d2a1255d0024106
Tamilarasideepsphere <87171608+Tamilarasideepsphere@users.noreply.github.com>
2021-07-09 15:20:26 +0530

    Initial commit

---
+++ home.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>LMS- DeepSphere</title>
+
+  
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+<link rel="stylesheet" href="css/style-live.css" >
+<link rel="stylesheet" href="fonts/fonts.css" >
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+
+    <!-- Custom styles for this template -->
+
+  </head>
+
+  <body class="LMS-Pge">
+
+<div class="home-sec">
+<div class="border-strip">
+<header class="logo-header">
+
+	<a class="logo-part" href=""><img src="images/deeplogo.png"></a>
+	</header>
+  <br/> 
+	<section class="LMS-cont text-center">
+        <h1 class="cover-heading">DeepSphere.AI <br>
+is the First and Only Comprehensive Personalized <br>
+<label>Learning Management Platform</label></h1>
+  
+        <p class="learn-btn">
+          <a href="https://deepsphereai.litmos.com/home/dashboard" class="btn btn-lg btn-secondary"><img src="images/boo.png"> Start Your Learning >></a>
+        </p>
+      </section>
+
+</div>
+</div>
+<br/><br/>
+
+      <footer class="footer">
+        <div class="inner">
+          <h3> Our iLMS Platform Empowered by SAP, AWS, and Google Cloud Technology Interoperability</h3>
+		    <div class="foo-images">
+			<div class="img-footer">
+		  <img src="images/sap2.png" class="img-fluid">
+		  </div>
+		  <div class="img-footer">
+		  <img src="images/lit1.png" class="img-fluid">
+		  </div>
+		  <div class="img-footer">
+		  <img src="images/ama1.png" class="img-fluid">
+		  </div>
+		  <div class="img-footer">
+		  <img src="images/goo.png" class="img-fluid">
+		  </div>
+		  </div>
+        </div>
+		<div class="shad">
+		  <div class="bg-left">
+	  <img src="images/leftshadow.png">
+</div>
+  <div class="bg-right">
+	  <img src="images/rightshadoww.png">
+</div>
+</div>
+      </footer>
+
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+
+    <title>LMS- DeepSphere</title>
+
+  
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" >
+<link rel="stylesheet" href="css/style.css" >
+<link rel="stylesheet" href="fonts/fonts.css" >
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+
+    <!-- Custom styles for this template -->
+
+  </head>
+
+  <body class="LMS-Pge">
+
+
+<div class="border-strip">
+<header class="logo-header">
+
+	<a class="logo-part" href=""><img src="images/deeplogo.png"></a>
+	</header>
+	<section class="LMS-cont text-center">
+        <h1 class="cover-heading">DeepSphere.AI <br>
+is the First and Only Comprehensive Personalized <br>
+<label>Learning Management Platform</label></h1>
+  
+        <p class="learn-btn">
+          <a href="#" class="btn btn-lg btn-secondary"><img src="images/book.png"> Start Your Learning >></a>
+        </p>
+      </section>
+
+</div>
+
+
+      
+	  <div class="bg-img">
+	  <img src="images/personimage.png" class="img-fluid">
+</div>
+      <footer class="footer">
+        <div class="inner">
+          <h3> Our iLMS Platform Empowered by SAP, AWS, and Google Cloud Technology Interoperability</h3>
+		  <img src="images/logo.png">
+        </div><br><br><br><br>
+		<div class="shad">
+		  <div class="bg-left">
+	  <img src="images/leftshadow.png">
+</div>
+  <div class="bg-right">
+	  <img src="images/rightshadoww.png">
+</div>
+</div>
+      </footer>
+
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
